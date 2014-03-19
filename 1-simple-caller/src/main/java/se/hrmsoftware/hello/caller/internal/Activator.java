83fcd81470087fae63525d1f446169f563b09a14
Anders Engström <anders.engstrom@hrmsoftware.se>
2014-03-19 21:29:10 +0100

    Add naive caller.

---
+++ 1-simple-caller/src/main/java/se/hrmsoftware/hello/caller/internal/Activator.java
@@ -0,0 +1,69 @@
+package se.hrmsoftware.hello.caller.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.hrmsoftware.hello.service.HelloService;
+
+import java.util.Arrays;
+
+/**
+ * Naive activator that looks up the service reference on
+ * activation.
+ */
+public class Activator implements BundleActivator {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		LOG.info("Starting Bundle");
+
+		// Get our name.
+		String myName = context.getBundle().getSymbolicName();
+
+		// Find reference.
+		ServiceReference<HelloService> ref = context.getServiceReference(HelloService.class);
+		if (ref != null) {
+			try {
+				// Print service properties.
+				String props = getServiceProperties(ref);
+				LOG.info("Calling HelloService#sayHello('{}') Published by {}. Props:\n{}",
+						myName, ref.getBundle().getSymbolicName(), props);
+				// Get the service
+				HelloService service = context.getService(ref);
+				if (service != null) {
+					service.sayHello(myName);
+				}
+			} finally {
+				// Release reference
+				context.ungetService(ref);
+			}
+		}
+	}
+
+	private String getServiceProperties(ServiceReference<HelloService> ref) {
+		StringBuilder builder = new StringBuilder();
+		for (String key : ref.getPropertyKeys()){
+			Object value = ref.getProperty(key);
+			String strValue;
+			if (value instanceof Object[]) {
+				strValue = Arrays.toString((Object[]) value);
+			} else {
+				strValue = value.toString();
+			}
+			if (builder.length() > 0) {
+				builder.append("\n");
+			}
+			builder.append("\t|").append(key).append(" : ").append(strValue);
+		}
+		return builder.toString();
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		LOG.info("Stopping Bundle");
+	}
+}
--- 1-simple-service/src/main/java/se/hrmsoftware/hello/internal/Activator.java
+++ 1-simple-service/src/main/java/se/hrmsoftware/hello/internal/Activator.java
@@ -4,6 +4,8 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.hrmsoftware.hello.service.HelloService;
 
 import java.util.Dictionary;
@@ -14,17 +16,20 @@ import java.util.Hashtable;
  */
 public class Activator implements BundleActivator {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
 	private ServiceRegistration<HelloService> serviceRegistration;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
-		Dictionary<String, Object> props = new Hashtable<>();
+		LOG.info("Starting Bundle and Registering service");
+		Dictionary<String, Object> props = new Hashtable<String, Object>();
 		props.put(Constants.SERVICE_DESCRIPTION, "Simple Hello World Service");
 		serviceRegistration = context.registerService(HelloService.class, new SimpleHelloService(), props);
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		LOG.info("Stopping Bundle and Unregistering service");
 		if (serviceRegistration != null) {
 			serviceRegistration.unregister();
 		}
